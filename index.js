d0edaffdbdeb474b88a9faefb4fd6cc5e13ea8a0
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-03-31 11:02:18 -0500

    activity 2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
+// FLujo de lectura
 const readable = fs.createReadStream('datos.txt', { encoding: 'utf8' });
 
 readable.on('data', chunk => console.log('Fragmento recibido:', chunk));
 readable.on('end', () => console.log('Lectura completa'));
 readable.on('error', err => console.error('Error:', err));
+
+// Flujo de escritura
+const writable = fs.createWriteStream('salida.txt');
+writable.write('Este es un mensaje de prueba.\n');
+writable.end('Fin del mensaje.');
+writable.on('finish', () => console.log('Escritura completada.'));
