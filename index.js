d1acb4625e8bf6ed7278bca46ce932d98f01ddd6
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-03-31 11:16:47 -0500

    Activity 3

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ const writable = fs.createWriteStream('salida.txt');
 writable.write('Este es un mensaje de prueba.\n');
 writable.end('Fin del mensaje.');
 writable.on('finish', () => console.log('Escritura completada.'));
+
+// Compresión del archivo entrada.txt
+const zlib = require('zlib');
+const readStream = fs.createReadStream('entrada.txt');
+const writeStream = fs.createWriteStream('entrada.txt.gz');
+const gzip = zlib.createGzip();
+
+readStream.pipe(gzip).pipe(writeStream);
